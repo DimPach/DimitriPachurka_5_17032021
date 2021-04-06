66777cced8ee48bee43eee8d6879f0509a35cbd4
DimPach <mrdimpac@hotmail.fr>
2021-04-06 13:38:18 +0200

    template pages confirm, panier etc

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 fetch('http://localhost:3000/api/cameras')
     .then(response => {
-        return response.json()
+        return response.json();
         
     })
     .then(datas => {
         for(data of datas){
-            console.log(datas)
-            displayApp(data)
+            console.log(datas);
+            displayMain(data);
         }
     })
     .catch(err => {
         console.log(err);
     })
 
-    const displayApp = (data) => {
-        const temp = document.querySelector('#templateApp')
-        const clone = document.importNode(templateApp.content, true)
+    const displayMain = (data) => {
+        const temp = document.querySelector('#templateApp');
+        const clone = document.importNode(templateApp.content, true);
 
-        clone.querySelector('.card-title').textContent = data.name
-        clone.querySelector('.card-text').textContent = data.description
-        clone.querySelector('.prix').textContent = "Prix : " + data.price + " €"
+        clone.querySelector('.card-title').textContent = data.name;
+        clone.querySelector('.card-text').textContent = data.description;
+        clone.querySelector('.prix').textContent = "Prix : " + data.price + " €";
 
-        for(len of data.lenses){
+        /*for(len of data.lenses){
             let newOption = document.createElement('option')
             newOption.textContent = len
             clone.querySelector('.form-select').appendChild(newOption)
-        }
+        }*/
         
-        clone.querySelector('img').setAttribute("src", `${data.imageUrl}`)
+        clone.querySelector('img').setAttribute("src", `${data.imageUrl}`);
 
-        document.querySelector('.row').appendChild(clone)
+        document.querySelector('.row').appendChild(clone);
     }
+
+    document.addEventListener(onclick, () => {
+
+    });
