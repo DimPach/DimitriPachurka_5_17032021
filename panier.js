0d09c4d287e118330a935a1d7c929fe1bf02710c
DimPach <mrdimpac@hotmail.fr>
2021-07-05 14:22:06 +0200

    modifs responsive

--- panier.js
+++ panier.js
@@ -39,7 +39,7 @@ for (let i = 0; i < localStorage.length; i++) {
       }
       
       totalPrice = totalPrice + actualPrice
-      newPrice.textContent = totalPrice + ".00" + ' €';
+      newPrice.textContent = totalPrice + ".00" + '€';
     })
   })
 }
